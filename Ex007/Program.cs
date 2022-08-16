1a1f5a888f7576597869122ca8b76158203d293c
Evgeny Simonenko <simonenko-e@yandex.ru>
2022-08-16 23:48:47 +0300

    Add Ex007

---
+++ Ex007/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Clear();
+
+int xa = 35, ya = 1,
+    xb = 1, yb = 25,
+    xc = 70, yc = 25;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("*");
+
+int x = xa, y = ya,
+    count = 0;
+
+while (count < 10000)
+{
+    int rnd = new Random().Next(0, 3);
+
+    if(rnd == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    
+    if(rnd == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if(rnd == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("*");
+
+    count++;
+
+
+}
