8cf7fd67b3eaedd3a182d32be67f80c17205f4d5
Evgeny Simonenko <simonenko-e@yandex.ru>
2022-08-16 22:28:44 +0300

    Add Ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter user name: ");
+string user_name = Console.ReadLine();
+if (user_name.ToLower() == "john")
+{
+    Console.WriteLine("Welcome John!");
+}
+else
+{
+Console.Write("Hello, ");
+Console.WriteLine(user_name);
+}
+
